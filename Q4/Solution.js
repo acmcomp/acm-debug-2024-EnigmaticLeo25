cfff1e9d5e880ffaba86ba88cd24316c1e4b2692
Ethan Dsa <ethandsa25@gmail.com>
2024-12-05 15:20:04 +0400

    solved q4

--- Q4/Solution.js
+++ Q4/Solution.js
@@ -1,20 +1,20 @@
 function rotate(nums, k) {
-    k = k % nums.length;
-    reverse(nums, 0, nums.length);
-    reverse(nums, 0, k);
-    reverse(nums, k, nums.length);
-    return nums;
+  k = k % nums.length;
+  reverse(nums, 0, nums.length - 1);
+  reverse(nums, 0, k - 1);
+  reverse(nums, k, nums.length - 1);
+  return nums;
 }
 
 function reverse(nums, start, end) {
-    while (start < end) {
-        let temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
-        start++;
-        end++;
-    }
+  while (start < end) {
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
 }
 
 console.log(rotate([1, 2, 3, 4, 5], 2));
-console.log(rotate([1, 2, 3], 4)); 
+console.log(rotate([1, 2, 3], 4));
